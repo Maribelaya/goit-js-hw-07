8eecb65b64b86a4a91241bae702d17981e4d6833
Maribelaya <shandermarina7@gmail.com>
2024-01-16 20:32:32 +0100

    hw7

--- js/task-5.js
+++ js/task-5.js
@@ -9,35 +9,6 @@ document.querySelector(".change-color").addEventListener("click", event => {
     document.body.style.backgroundColor = color;
     document.querySelector(".color").textContent = color;
 
-    //document.querySelector("body").setAttribute("style", "background-color: " + color);
-    //document.querySelector("span.color").textContent = color;
-
 });
 
-// const changeColorButton = document.querySelector(".change-color");
-// const widget = document.querySelector(".widget");
-// const colorSpan = document.querySelector(".color");
 
-// changeColorButton.addEventListener("click", () => {
-//   const randomColor = getRandomHexColor();
-//   document.body.style.backgroundColor = randomColor;
-//   widget.style.backgroundColor = randomColor;
-//   colorSpan.textContent = randomColor;
-// });
-
-
-
-// Напиши скрипт, який змінює колір фону елемента <body> через інлайн-стиль по кліку на button.change-color і
-//  задає це значення кольору текстовим вмістом для span.color.
-
-// <div class="widget">
-//   <p>Background color: <span class="color">-</span></p>
-//   <button type="button" class="change-color">Change color</button>
-// </div>
-
-// Для генерування випадкового кольору використовуй функцію getRandomHexColor().
-
-
-
-// Зверни увагу, що функція getRandomHexColor() повертає колір у hex-форматі, в той час як 
-// колір фону на <body> буде у форматі rgb. Це нормально й не потребує якихось правок.
